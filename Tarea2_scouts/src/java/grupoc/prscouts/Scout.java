a256cfb238edf8e8bcaf9e945c6bc433b9736282
eloygm <eloygm.92@hotmail.com>
2018-05-09 17:18:36 +0200

    bd añadida

---
+++ Tarea2_scouts/src/java/grupoc/prscouts/Scout.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupoc.prscouts;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Jose Luis Ruiz 
+ */
+@Entity
+@PrimaryKeyJoinColumn(name = "id")
+public class Scout extends Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Column (nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date fecha_titulo;
+    private boolean coordinador;
+    @OneToMany (mappedBy = "id_scout")
+    private List<Pago> pagos;
+    @ManyToOne 
+    private Seccion seccion;
+            
+
+
+    public Date getFecha_titulo() {
+        return fecha_titulo;
+    }
+
+    public void setFecha_titulo(Date fecha_titulo) {
+        this.fecha_titulo = fecha_titulo;
+    }
+
+    public boolean isCoordinador() {
+        return coordinador;
+    }
+
+    public void setCoordinador(boolean coordinador) {
+        this.coordinador = coordinador;
+    }
+
+    @Override
+    public String toString() {
+        return "Scout{" + "fecha_titulo=" + fecha_titulo + ", coordinador=" + coordinador + ", pagos=" + pagos + ", seccion=" + seccion + '}';
+    }
+
+    
+
+
+}
---
+++ Tarea2_scouts/src/java/grupoc/prscouts/TiposDocumento.java
@@ -0,0 +1,24 @@
+package grupoc.prscouts;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Eloy García
+ */
+public enum TiposDocumento {
+    PDF,
+    TXT,
+    DOC,
+    XDOC,
+    ODT,
+    JPEG,
+    JPG,
+    PNG,
+    ZIP,
+    RAR
+}
